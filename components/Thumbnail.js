fac9666d4351627e7fd238cb0be7e15838fcb3b7
Hung Nguyen <hungnguyen@Hungs-MBP-2.attlocal.net>
2022-02-15 19:09:24 -0600

    change title release date to US format and replace vote count with audience score

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -5,30 +5,32 @@ import { forwardRef } from "react";
 const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original";
 
+  const releaseDate = new Date(result.release_date || result.first_air_date);
+  const formattedReleaseDate = new Intl.DateTimeFormat("en-US").format(
+    releaseDate
+  );
+
   return (
     <div
       ref={ref}
-      className="p-4 group cursor-pointer duration-200 ease-in transform sm:hover:scale-105 lg:p-4 hover:z-50"
+      className="p-2  md:p-4 group cursor-pointer duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
     >
       <Image
         alt="Image of movie poster"
         layout="responsive"
-        src={
-          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-          `${BASE_URL}${result.poster_path}`
-        }
+        src={`${BASE_URL}${result.backdrop_path || result.poster_path}`}
         height={1080}
         width={1920}
       />
-      <div className="p-2">
-        <p className="line-clamp-2">{result.overview}</p>
-        <h2 className="mt-2 text-white text-2xl duration-150 ease-in-out group-hover:font-bold">
+      <div className="mt-3 ml-1">
+        <p className="line-clamp-2 text-sm">{result.overview}</p>
+        <h2 className="mt-2 text-white text-[1.3em] duration-150 ease-in-out group-hover:font-medium">
           {result.title || result.original_title || result.name}
         </h2>
-        <p className="flex items-center opacity-0 group-hover:opacity-100">
-          {/* {result.media_type && `${result.media_type} •`}{" "} */}
-          {result.release_date || result.first_air_date} •{" "}
-          <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
+        <p className="flex items-center text-sm opacity-0 group-hover:opacity-100">
+          {formattedReleaseDate}
+          <ThumbUpIcon className="h-5 mx-2" />{" "}
+          {`Audience Score: ${result.vote_average}/10`}
         </p>
       </div>
     </div>
